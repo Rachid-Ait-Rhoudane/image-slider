f1cfbf257406add9a9be38a0a3140938c57aec69
Rachid-Ait-Rhoudane <rachid.aitrhoudane@etu.uae.ac.ma>
2023-09-11 13:20:17 +0100

    add slider JS logique and pagination

--- js/index.js
+++ js/index.js
@@ -1,1 +1,79 @@
-console.log("preparing environment");
+let sliderImages = Array.from(document.querySelectorAll(".slider-wrapper .slider-images img"));
+
+let numberOfImages = sliderImages.length;
+
+// let indicatorsList = document.createElement("ul");
+
+// for(let i = 1; i <= numberOfImages; i++) {
+//     let indicator = document.createElement("li");
+//     indicator.id = i;
+//     let indicatorTxt = document.createTextNode(i);
+//     indicator.appendChild(indicatorTxt);
+//     indicatorsList.appendChild(indicator);
+// }
+
+// document.querySelector(".slider-pagination .indicators").appendChild(indicatorsList);
+
+let currentShownImage = 1;
+
+// indicatorsList.children[currentShownImage-1].classList.add("active");
+
+let currentShownImageIndex = document.querySelector(".slider-wrapper .slider-number");
+
+let imageSliderWrapper = document.querySelector(".slider-wrapper .slider-images");
+
+let sliderNextButton = document.querySelector(".slider-wrapper .slider-controls .next");
+
+let sliderPreviousButton = document.querySelector(".slider-wrapper .slider-controls .previous");
+
+sliderNextButton.addEventListener("click", function () {
+    if (currentShownImage <  numberOfImages) {
+        currentShownImage++;
+        currentShownImageIndex.innerText = `Slide #${currentShownImage}`;
+        imageSliderWrapper.style.left = `-${(currentShownImage-1)*100}%`;
+    } 
+    DisableOrActive();
+});
+
+sliderPreviousButton.addEventListener("click", function () {
+    if (1 < currentShownImage) {
+        currentShownImage--;
+        currentShownImageIndex.innerText = `Slide #${currentShownImage}`;
+        imageSliderWrapper.style.left = `-${(currentShownImage-1)*100}%`;
+    }
+    DisableOrActive();
+} );
+
+// for(let li of indicatorsList.children) {
+//     li.addEventListener("click", function () {
+//         currentShownImage = +li.innerText;
+//         imageSliderWrapper.style.left = `-${(currentShownImage-1)*100}%`;
+//         DisableOrActive();
+//     });
+// }
+
+function DisableOrActive() {
+
+    if(1 <= currentShownImage && currentShownImage <=  numberOfImages) {
+        if (sliderNextButton.classList.contains("disabled")) {
+            sliderNextButton.classList.remove("disabled");
+        } else if(sliderPreviousButton.classList.contains("disabled")) {
+            sliderPreviousButton.classList.remove("disabled");
+        }
+    } 
+    
+    if (currentShownImage == 1) {
+        sliderPreviousButton.classList.add("disabled");
+    } 
+    
+    if (currentShownImage ==  numberOfImages) {
+        sliderNextButton.classList.add("disabled");
+    }
+
+    // for(let li of indicatorsList.children) {
+    //     li.classList.remove("active");
+    // }
+
+    // indicatorsList.children[currentShownImage-1].classList.add("active");
+}
+
